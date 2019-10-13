fb140d08cb030c0ac16b7e0c4f308b7962567d44
Dima <v3il@ukr.net>
2019-10-13 20:31:55 +0300

    Js check, task 1

---
+++ js-check/1.js
@@ -0,0 +1,12 @@
+const my_number = -1;
+var myNumber = 0;
+var number = 3;
+
+if (true) {
+    const my_number = 1;
+    var myNumber = 2;
+    var number = 6;
+}
+
+const sum = my_number + myNumber + number; // Result: -1 + 2 + 6 = 7
+console.log(sum);
