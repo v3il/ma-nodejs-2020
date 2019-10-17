568d5dbb56f8e3735cb6964d1a7c8e53bb1970a6
Dima <v3il@ukr.net>
2019-10-17 18:16:13 +0300

    Tests for task 10

--- js-check/10.js
+++ js-check/10.js
@@ -1,17 +1,3 @@
-// Дано клас Storage, що є SDK для доступу до якогось сховища даних по текстовому ключу. При цьому,
-//     кожен метод повертає Promise:
-//     Storage
-// list() повертає Promise, який зарезолвиться масивом ключів
-// fetch(key) повертає Promise, який зарезолвиться даними
-// store(key, data) повертає Promise
-// destroy(key) повертає Promise
-// Потрібно реалізувати нові методи (що також повертатимуть Promise):
-// storeList([{key, data}])
-// destroyStartedWith(beginningOfKey)
-// fetchInTimeOrFail(key, timeout)
-// Додаткова умова (не обов’язково, за бажанням):
-// Сховище має ліміт на записування та читання по 5 операцій одночасно для одного клієнта.
-
 class Storage {
     constructor() {
         this.storage = new Map();
@@ -42,8 +28,7 @@ class Storage {
 
     async destroyStartedWith(beginningOfKey) {
         const allKeys = await this.list();
-        const beginningOfKeyIndex = allKeys.findIndex(item => item === beginningOfKey);
-        const keysToRemove = allKeys.splice(beginningOfKeyIndex);
+        const keysToRemove = allKeys.filter(key => `${key}`.startsWith(beginningOfKey));
 
         for (const key of keysToRemove) {
             await this.destroy(key);
@@ -65,12 +50,4 @@ class Storage {
     }
 }
 
-(async () => {
-    const s = new Storage();
-    s.store(1, 1);
-    s.storeList([{key: 3, data: 2}, {key: 5, data: 1}]);
-    console.log(await s.list(2));
-    // await s.destroyStartedWith(3);
-    console.log(await s.fetchInTimeOrFail(3, 4000))
-    console.log(await s.list());
-})();
+module.exports = Storage;
---
+++ js-check/__tests__/10.spec.js
@@ -0,0 +1,141 @@
+const Storage = require('../10');
+
+describe('Storage', () => {
+    it('creates a storage properly', () => {
+        const storage = new Storage();
+        expect(storage.storage instanceof Map).toBe(true);
+    });
+
+    it('sets data correctly', async () => {
+        const storage = new Storage();
+
+        await storage.store(null, 1);
+        await storage.store(false, 2);
+        await storage.store('test', 2);
+        await storage.store(Infinity, 2);
+        await storage.store(NaN, 2);
+
+        const objKey = {};
+        await storage.store(objKey, 2);
+
+        expect(storage.storage.size).toBe(6);
+        expect(storage.storage.get(null)).toBe(1);
+        expect(storage.storage.get(false)).toBe(2);
+        expect(storage.storage.get('test')).toBe(2);
+        expect(storage.storage.get(Infinity)).toBe(2);
+        expect(storage.storage.get(NaN)).toBe(2);
+        expect(storage.storage.get(objKey)).toBe(2);
+    });
+
+    it('returns a list of keys correctly', async () => {
+        const storage = new Storage();
+
+        await storage.store(null, 1);
+        await storage.store(false, 2);
+        await storage.store('test', 2);
+        await storage.store(Infinity, 2);
+        await storage.store(NaN, 2);
+
+        expect(storage.list()).resolves.toStrictEqual([null, false, 'test', Infinity, NaN]);
+    });
+
+    it('returns data by key correctly', async () => {
+        const storage = new Storage();
+
+        await storage.store(null, 1);
+        await storage.store(false, 2);
+        await storage.store('test', 2);
+        await storage.store(Infinity, 2);
+        await storage.store(NaN, 2);
+
+        return expect(Promise.all([
+            storage.fetch('test'),
+            storage.fetch(null),
+            storage.fetch(false),
+            storage.fetch(Infinity),
+            storage.fetch(NaN),
+        ])).resolves.toStrictEqual([2, 1, 2, 2, 2]);
+    });
+
+    it('removes data by key correctly', async () => {
+        const storage = new Storage();
+
+        await storage.store(null, 1);
+        await storage.store(false, 2);
+        await storage.store('test', 2);
+        await storage.store(Infinity, 2);
+        await storage.store(NaN, 2);
+
+        expect(storage.storage.size).toBe(5);
+
+        await storage.destroy('test');
+        expect(storage.storage.size).toBe(4);
+        expect(storage.storage.has('test')).toBe(false);
+
+        await storage.destroy(null);
+        expect(storage.storage.size).toBe(3);
+        expect(storage.storage.has(null)).toBe(false);
+
+        await storage.destroy(null);
+        expect(storage.storage.size).toBe(3);
+    });
+
+    it('sets a list of data correctly', async () => {
+        const storage = new Storage();
+
+        await storage.storeList([
+            { key: null, data: 1 },
+            { key: 'test', data: {} },
+            { key: 'blah', data: [] },
+        ]);
+
+        expect(storage.storage.size).toBe(3);
+
+        return expect(Promise.all([
+            storage.fetch('test'),
+            storage.fetch('blah'),
+            storage.fetch(null),
+        ])).resolves.toStrictEqual([{}, [], 1]);
+    });
+
+    it('removes data by beginning of keys correctly', async () => {
+        const storage = new Storage();
+
+        await storage.storeList([
+            { key: null, data: 1 },
+            { key: 'test', data: 2 },
+            { key: 'null2', data: 3 },
+            { key: 'null4', data: 4 },
+            { key: false, data: 5 },
+        ]);
+
+        expect(storage.storage.size).toBe(5);
+
+        await storage.destroyStartedWith(null);
+
+        expect(storage.storage.size).toBe(2);
+
+        return expect(storage.list()).resolves.toStrictEqual(['test', false]);
+    });
+
+    it('return data by key after setTimeout correctly', async () => {
+        const storage = new Storage();
+
+        await storage.storeList([
+            { key: null, data: 1 },
+            { key: 'test', data: 2 },
+            { key: 'null2', data: 3 },
+            { key: 'null4', data: 4 },
+            { key: false, data: 5 },
+        ]);
+
+        const data = await storage.fetchInTimeOrFail('test', 500);
+        expect(data).toBe(2);
+
+        try {
+            await storage.fetchInTimeOrFail('test-123', 500);
+        } catch (data) {
+            expect(data).toBe(undefined);
+        }
+    });
+});
