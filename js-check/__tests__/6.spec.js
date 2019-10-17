f275f177f08f23e8d3dd6d8973dfca2f3905dd79
Dima <v3il@ukr.net>
2019-10-17 20:13:50 +0300

    Added some tests for task 6

--- js-check/__tests__/6.spec.js
+++ js-check/__tests__/6.spec.js
@@ -8,5 +8,8 @@ describe('6.js', () => {
         expect(getFlatArray([1, 2, 3, 4, 5], [6, 7, 8, 9, 0])).toStrictEqual([5, 0, 4, 9, 3, 8, 2, 7, 1, 6]);
         expect(getFlatArray([1, 2], [3, 4], [5, 6])).toStrictEqual([2, 4, 6, 1, 3, 5]);
         expect(getFlatArray(['a', 'b'], ['c', 'd'], [1, false])).toStrictEqual(['b', 'd', false, 'a', 'c', 1]);
+        expect(getFlatArray([1, 2])).toStrictEqual([2, 1]);
+        expect(getFlatArray([1])).toStrictEqual([1]);
+        expect(getFlatArray([])).toStrictEqual([]);
     });
 });
