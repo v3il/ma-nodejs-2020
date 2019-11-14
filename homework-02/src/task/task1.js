4347591108b867fd5997ef9f96900c8f725d5a14
Dima <v3il@ukr.net>
2019-11-14 14:37:56 +0200

    Fixed task 1

--- homework-02/src/task/task1.js
+++ homework-02/src/task/task1.js
@@ -1,3 +1,13 @@
+const my_number = -1;
+var myNumber = 0;
+var number = 3;
+
+if (true) {
+    const my_number = 1;
+    var myNumber = 2;
+    var number = 6;
+}
+
 const getSum = (first, second, third) => first + second + third;
 
-module.exports = getSum(-1, 2, 6);
+module.exports = getSum(my_number, myNumber, number);
