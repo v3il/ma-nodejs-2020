3dd549627fed9f32dec6419d4bed9d8f250cc0db
Dima <v3il@ukr.net>
2019-11-18 19:25:07 +0200

    Wrapped call of task3 with try-catch

--- homework-02/src/app.js
+++ homework-02/src/app.js
@@ -3,8 +3,12 @@ const { task1: sum, task2: earthInstance, task3 } = require('./task');
 const boot = async () => {
     console.log(sum);
 
-    const result = await task3;
-    console.log(result);
+    try {
+        const result = await task3;
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 
     console.log(earthInstance.describe());
 };
