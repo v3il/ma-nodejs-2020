8ae3ef45fc6125be7af49c9150e027b11d686deb
Dima <v3il@ukr.net>
2019-11-27 23:59:38 +0200

    Algorithm optimization for big numbers

--- homework-03/src/index.js
+++ homework-03/src/index.js
@@ -1,15 +1,14 @@
 const PrimeNumbersFinder = require('./PrimeNumbersFinder');
 
 function run() {
-    const primeNumbersFinder = new PrimeNumbersFinder();
+    const primeNumbersFinder = new PrimeNumbersFinder(999999999999999);
 
     setInterval(() => {
         const timestamp = (Date.now() / 1000).toFixed(0);
-        const usedMemory = process.memoryUsage().heapUsed;
         const biggestFoundNumber = primeNumbersFinder.getBiggestFoundNumber();
 
         console.log(
-            `${timestamp}: -- IN PROCESS -- Biggest prime number found: ${biggestFoundNumber}. Memory used: ${usedMemory}`,
+            `${timestamp}: -- IN PROCESS -- Biggest prime number found: ${biggestFoundNumber}`,
         );
     }, 1000);
 }
