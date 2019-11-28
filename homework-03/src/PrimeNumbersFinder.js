dfc45efb581f2eabca1a673baef4d8669c437044
Dima <v3il@ukr.net>
2019-11-28 16:40:07 +0200

    Split for-loop into small iterations

--- homework-03/src/PrimeNumbersFinder.js
+++ homework-03/src/PrimeNumbersFinder.js
@@ -1,28 +1,56 @@
+const { performance } = require('perf_hooks');
+
+const INITIAL_DIVIDER = 3;
+const CYCLE_MAX_DURATION = 50;
+
 module.exports = class PrimeNumbersFinder {
     constructor(startNumber = 1) {
-        const INITIAL_DIVIDER = 2;
-
         this.biggestFoundNumber = 0;
+        this.numberToCheck = startNumber;
+        this.latestCheckedDivider = INITIAL_DIVIDER;
 
-        let currentDivider = INITIAL_DIVIDER;
-        let numberToCheck = startNumber;
+        this.runNextTick();
+    }
 
-        setInterval(() => {
-            const allDividersChecked = currentDivider > Math.sqrt(numberToCheck);
-            const currentNumberIsNotPrime = numberToCheck % currentDivider === 0;
+    tick() {
+        const start = performance.now();
 
-            if (allDividersChecked) {
-                this.biggestFoundNumber = numberToCheck;
+        // Even numbers are not prime
+        if (this.numberToCheck % 2 === 0) {
+            this.moveToNextNumber();
+            return this.runNextTick();
+        }
+
+        // Check odd numbers only
+        for (let i = this.latestCheckedDivider; i <= Math.sqrt(this.numberToCheck); i += 2) {
+            // Time for this iteration is out
+            if (performance.now() - start >= CYCLE_MAX_DURATION) {
+                this.latestCheckedDivider = i;
+                return this.runNextTick(CYCLE_MAX_DURATION * 2);
             }
 
-            if (allDividersChecked || currentNumberIsNotPrime) {
-                currentDivider = INITIAL_DIVIDER;
-                numberToCheck += 1;
-            } else {
-                // Skip all even numbers greater than 2
-                currentDivider += currentDivider === INITIAL_DIVIDER ? 1 : 2;
+            // This number is not prime
+            if (this.numberToCheck % i === 0) {
+                this.moveToNextNumber();
+                return this.runNextTick();
             }
-        }, 0);
+        }
+
+        // This number is prime
+        this.biggestFoundNumber = this.numberToCheck;
+        this.moveToNextNumber();
+        return this.runNextTick();
+    }
+
+    runNextTick(delay = 0) {
+        setTimeout(() => {
+            this.tick();
+        }, delay);
+    }
+
+    moveToNextNumber() {
+        this.latestCheckedDivider = INITIAL_DIVIDER;
+        this.numberToCheck += 1;
     }
 
     getBiggestFoundNumber() {
