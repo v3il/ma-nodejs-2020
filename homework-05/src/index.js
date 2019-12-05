6a900f8520f213dadcb8e7593395347fb3465338
Dima <v3il@ukr.net>
2019-12-05 10:16:46 +0200

    Homework 05

--- homework-05/src/index.js
+++ homework-05/src/index.js
@@ -9,41 +9,85 @@ const gzip = promisify(zlib.gzip);
 const inputDirName = 'input';
 const outputDirName = 'output';
 const outputFileName = 'result.json.gz';
+const outputTextFileName = 'result.json';
 
 const inputDir = path.join(process.cwd(), inputDirName);
 const outputFile = path.join(process.cwd(), outputDirName, outputFileName);
+const outputTextFile = path.join(process.cwd(), outputDirName, outputTextFileName);
 
 async function getInputFileList() {
-    const fileNames = await fsp.readdir(inputDir);
+    let fileNames = [];
+
+    try {
+        fileNames = await fsp.readdir(inputDir);
+    } catch (error) {
+        console.error(`Can't read the directory ${inputDir}`);
+    }
+
     return fileNames.map(fileName => path.join(inputDir, fileName));
 }
 
 async function getObjectFromFile(filePath) {
-    const compressedBuffer = await fsp.readFile(filePath);
-    const buffer = await gunzip(compressedBuffer);
-    return JSON.parse(buffer);
+    let compressedBuffer;
+
+    try {
+        compressedBuffer = await fsp.readFile(filePath);
+    } catch (error) {
+        console.error(`An error occurred while reading the file "${filePath}"`, error);
+    }
+
+    let buffer;
+
+    try {
+        buffer = await gunzip(compressedBuffer);
+    } catch (error) {
+        console.error('An error occurred during data decompression', error);
+    }
+
+    try {
+        return JSON.parse(buffer);
+    } catch (error) {
+        console.error(`File "${filePath}" contains invalid JSON data`);
+    }
+
+    return {};
 }
 
 function rebuildUrl(originalUrl) {
-    console.log(originalUrl);
-  // Change protocol, path, search string of URL
-  // use URL class
-  // Example:
-  // from URL: http://example.com/files/devices/keyboards.xls
-  // to URL: https://example.com/devices?file=keyboards&type=.xls
+    const parsedURL = new URL(originalUrl);
+    parsedURL.protocol = 'https';
+    parsedURL.pathname = 'devices';
+
+    const { ext, name } = path.parse(originalUrl);
+    parsedURL.searchParams.append('file', name);
+    parsedURL.searchParams.append('type', ext);
+
+    return parsedURL.toString();
 }
 
 async function buildOutputObject(files) {
     const devices = {};
 
+    // eslint-disable-next-line
     for (const filePath of files) {
-        const fileObject = await getObjectFromFile(filePath);
+        let fileObject;
+
+        try {
+            // eslint-disable-next-line
+            fileObject = await getObjectFromFile(filePath);
+        } catch (error) {
+            console.error(
+                `An error occurred while reading the content of file "${filePath}"`,
+                error,
+            );
+        }
+
         fileObject.url = rebuildUrl(fileObject.url);
 
         const ext = '.json.gz';
-        const fileName = path.basename(filePath.toLowerCase(), ext);
+        const name = path.basename(filePath.toLowerCase(), ext);
 
-        devices[fileName] = fileObject;
+        devices[name] = fileObject;
     }
 
     return devices;
@@ -52,15 +96,32 @@ async function buildOutputObject(files) {
 async function saveOutput(object) {
     const jsonString = JSON.stringify(object);
     const uncompressedBuffer = Buffer.from(jsonString);
-    const compressedBuffer = await gzip(uncompressedBuffer);
 
-    await fsp.writeFile(outputFile, compressedBuffer);
+    let compressedBuffer;
+
+    try {
+        compressedBuffer = await gzip(uncompressedBuffer);
+    } catch (error) {
+        console.error('An error occurred during data compression', error);
+    }
+
+    try {
+        await fsp.writeFile(outputFile, compressedBuffer);
+    } catch (error) {
+        console.error(`An error occurred while writing the file "${outputFileName}"`, error);
+    }
+
+    try {
+        await fsp.writeFile(outputTextFile, JSON.stringify(object, null, 4));
+    } catch (error) {
+        console.error(`An error occurred while writing the file "${outputTextFileName}"`, error);
+    }
 }
 
 async function start() {
-  const inputFiles = await getInputFileList();
-  const outputObject = await buildOutputObject(inputFiles);
-  await saveOutput(outputObject);
+    const inputFiles = await getInputFileList();
+    const outputObject = await buildOutputObject(inputFiles);
+    await saveOutput(outputObject);
 }
 
 start().catch(err => console.error('🐞  🤪  🐛\n', err));
