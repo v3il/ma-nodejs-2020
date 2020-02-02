6f78d53302c1a66c66481dc4e643c69b90b891d7
Dima <v3il@ukr.net>
2020-02-02 20:11:26 +0200

    Games with streams :)

--- homework-09/src/server/Transformer.js
+++ homework-09/src/server/Transformer.js
@@ -4,39 +4,44 @@ module.exports = class Transformer extends Transform {
     constructor(limit = 1000) {
         super();
         this.limit = limit;
-        this.fullData = Buffer.from('');
         this.i = 0;
 
         this.transferedData = 0;
+        this.unsentData = null;
 
-        setInterval(() => {
+        const id = setInterval(() => {
             this.transferedData = 0;
             if (this.isPaused()) this.resume();
         }, 1000);
+
+        this.on('end', () => {
+            clearInterval(id);
+            console.log(this.i);
+        });
 
         console.log(limit);
     }
 
     // eslint-disable-next-line class-methods-use-this,no-underscore-dangle
-    _transform(chunk, encoding, callback) {
-        // this.pause();
+    _transform(chunk, encoding, done) {
+        const maxBytes = 1024 * 1024;
+
+        if (this.isPaused()) {
+            console.log('Paused');
+        }
+
+        // eslint-disable-next-line no-plusplus
+        this.i++;
+
+        // console.log(this.transferedData)
 
         this.transferedData += chunk.length;
 
-        if (this.transferedData > 1024 ** 2) {
+        if (this.transferedData > maxBytes) {
+            console.log('Pause');
             this.pause();
         }
 
-        // const delta = 1024;
-
-        // console.log(this.i * delta, (this.i + 1) * delta)
-
-        this.fullData += chunk;
-
-        // const a = this.fullData.slice(this.i * delta, (this.i + 1) * delta);
-
-        // this.i++;
-
-        callback(null, chunk);
+        done(null, chunk);
     }
 };
