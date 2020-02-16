d81aac100c578d6edcb948aa62af6658c19ff202
Dima <v3il@ukr.net>
2020-02-16 14:33:54 +0200

    Homework 11

--- homework-11/src/app.js
+++ homework-11/src/app.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 
 const http = require('http');
+const { sequelize } = require('./models');
 
 require('./prototypeExtensions');
 
 const Router = require('./Router');
 const setupRoutes = require('./routes');
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log('Unable connect to DB:', error.message);
+    }
+})();
 
 const router = new Router();
 
